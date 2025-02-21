ae9b78db5d04337ff770d97aba85ef8c66ac7ce3
Thomas <przegralek.thomas@gmail.com>
2025-02-21 16:03:58 +0100

    Suppression du test pour les attentes Selenium

--- tests/seleniumWait.spec.ts
+++
@@ -1,8 +0,0 @@
-import {test, expect} from '@playwright/test'
-
-test("Alert Button", async({page}) => {
-    await page.goto('https://qatraining.fr/pages/features/selenium-wait.html')
-    await page.getByRole('button', {name:'Simuler un Selenium Wait'}).click()
-    await page.waitForTimeout(3000)
-    expect(await page.locator('#status-message').innerText()).toBe('Attente terminée. Action réussie !')
-})
