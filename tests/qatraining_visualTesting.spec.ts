70f1a40f8f948069282e10fdef0b30b405ef4027
Thomas <przegralek.thomas@gmail.com>
2025-02-21 16:00:16 +0100

    Ajout du test pour simuler afficher/masquer une image

---
+++ tests/qatraining_visualTesting.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('Visual Test', async ({ page }) => {
+    // Aller à la page cible
+    await page.goto('https://qatraining.fr/pages/features/visual-testing.html');
+    // Vérifier que l'image est cachée
+    await expect(page.getByRole('img', { name: 'Logo de Facebook' })).toBeHidden();
+    // Cliquer sur le bouton pour afficher l'image
+    await page.getByRole('button', { name: "Afficher l'image" }).click();
+    // Attendre que l'image soit visible
+    await page.waitForSelector('img[alt="Logo de Facebook"]');
+    // Vérifier que l'image est visible
+    await expect(page.getByRole('img', { name: 'Logo de Facebook' })).toBeVisible();
+});
