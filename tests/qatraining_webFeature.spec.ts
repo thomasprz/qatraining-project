4f0d375bbfd6b2046e9851343e74758fe7ecf2e7
Thomas <przegralek.thomas@gmail.com>
2025-02-21 16:01:19 +0100

    Ajout du test pour simuler des fonctionnalités comme changement de thème, afficher/masquer un contenu ou bien démarrer une minuterie

---
+++ tests/qatraining_webFeature.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('Web Feature', async ({ page }) => {
+    // Aller à la page cible
+    await page.goto('https://qatraining.fr/pages/features/web-feature.html');
+
+    await page.getByRole('button', { name: "Changer le thème" }).click();
+    await expect(page.locator('body')).toHaveClass('dark-theme');
+    await expect(page.locator('body')).toHaveCSS('background-color', 'rgb(18, 18, 18)');
+    await expect(page.locator('#content-box > p')).toBeHidden()
+    await page.getByRole('button', { name: "Afficher/Masquer le contenu" }).click();
+    await expect(page.locator('#content-box > p')).toBeVisible()
+    await page.getByRole('button', { name: "Démarrer la minuterie" }).click();
+    await expect(page.locator('#timer')).toHaveText('00:00');
+});
