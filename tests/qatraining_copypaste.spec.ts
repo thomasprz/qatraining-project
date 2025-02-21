02c67eb3bc22ab30d201d646755b071eb8e17a9c
Thomas <przegralek.thomas@gmail.com>
2025-02-21 16:04:11 +0100

    Mise à jour du test pour le copier-coller

--- tests/qatraining_copypaste.spec.ts
+++ tests/qatraining_copypaste.spec.ts
@@ -1,24 +1,23 @@
-import {test, expect} from '@playwright/test'
+// import {test, expect} from '@playwright/test'
 
-test("Alert Button", async({page}) => {
-    // Gérer les dialogues (alertes) dans le navigateur
-    page.on('dialog', async dialog => {
-        if (dialog.message().includes('Texte copié dans le presse-papier !')) {
-            expect(dialog.type()).toContain('alert')
-            expect(dialog.message()).toContain('Texte copié dans le presse-papier !')
-            await dialog.accept()
-        } else if (dialog.message().includes('Texte collé avec succès !')) {
-            expect(dialog.type()).toContain('alert')
-            expect(dialog.message()).toContain('Texte collé avec succès !')
-        }
-    })
+// test("Alert Button", async({page}) => {
+//    page.on('dialog', async dialog => {
+//        if (dialog.message().includes('Texte copié dans le presse-papier !')) {
+//            expect(dialog.type()).toContain('alert')
+//            expect(dialog.message()).toContain('Texte copié dans le presse-papier !')
+//            await dialog.accept()
+//        } else if (dialog.message().includes('Texte collé avec succès !')) {
+//            expect(dialog.type()).toContain('alert')
+//            expect(dialog.message()).toContain('Texte collé avec succès !')
+//        }
+//    })
+// 
+//    await page.goto('https://qatraining.fr/pages/features/copy-paste.html')
 
-    await page.goto('https://qatraining.fr/pages/features/copy-paste.html')
-
-    await page.locator("#copy-field").fill("Bonjour")
-    await page.getByRole('button', {name: "Copier"}).click()
-    const pasteField = page.locator("#paste-area")
-    await pasteField.click()
-    await page.keyboard.press("Control+V")
-})
+//    await page.locator("#copy-field").fill("Bonjour")
+//    await page.getByRole('button', {name: "Copier"}).click()
+//     const pasteField = page.locator("#paste-area")
+//     await pasteField.click()
+//     await page.keyboard.press("Control+V")
+// })
 
