5e627c0b6085a6d3166d1eb30d16b5f7f5189738
Thomas <przegralek.thomas@gmail.com>
2025-03-15 16:06:23 +0100

    Ajout de tests pour la fonctionnalité de copier-coller

--- tests/qatraining_copypaste.spec.ts
+++ tests/qatraining_copypaste.spec.ts
@@ -1,23 +1,14 @@
-// import {test, expect} from '@playwright/test'
+import {test, expect} from '@playwright/test'
 
-// test("Alert Button", async({page}) => {
-//    page.on('dialog', async dialog => {
-//        if (dialog.message().includes('Texte copié dans le presse-papier !')) {
-//            expect(dialog.type()).toContain('alert')
-//            expect(dialog.message()).toContain('Texte copié dans le presse-papier !')
-//            await dialog.accept()
-//        } else if (dialog.message().includes('Texte collé avec succès !')) {
-//            expect(dialog.type()).toContain('alert')
-//            expect(dialog.message()).toContain('Texte collé avec succès !')
-//        }
-//    })
-// 
-//    await page.goto('https://qatraining.fr/pages/features/copy-paste.html')
+ test("Alert Button", async({page}) => {
+    await page.goto('/pages/features/copy-paste.html')
 
-//    await page.locator("#copy-field").fill("Bonjour")
-//    await page.getByRole('button', {name: "Copier"}).click()
-//     const pasteField = page.locator("#paste-area")
-//     await pasteField.click()
-//     await page.keyboard.press("Control+V")
-// })
+    await page.locator("#copy-field").fill("Bonjour");
+    await page.locator("#copy-field").press('ControlOrMeta+a');
+    await page.locator("#copy-field").press('ControlOrMeta+c');
 
+    await page.locator("#paste-area").click();
+    await page.locator("#paste-area").press('ControlOrMeta+v');
+
+    await expect(page.locator("#paste-area")).toHaveValue("Bonjour");
+})
