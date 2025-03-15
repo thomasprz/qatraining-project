0b0c9f3bb2470fe6e584a43256c5ab9a588de4f5
Thomas <przegralek.thomas@gmail.com>
2025-03-15 16:07:28 +0100

    Ajout de tests pour la fonctionnalité upload

--- tests/qatraining_fileupload.spec.ts
+++ tests/qatraining_fileupload.spec.ts
@@ -1,10 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test('File Upload', async ({ page }) => {
-    const filePath = '/Users/thomasprz/Documents/SiteInternet/Assets/Logo.jpg';
-
-    await page.goto('https://qatraining.fr/pages/features/file-upload.html');
-    await page.locator('#file-upload').click();
+    const filePath = 'assets/playwright.svg';
+    await page.goto('/pages/features/file-upload.html');
     await page.locator('#file-upload').setInputFiles(filePath);
-    await page.getByText('Fichier téléchargé avec succè').click();
+    await expect(page.getByText('Fichier téléchargé avec succès')).toBeVisible();
+    await page.screenshot({ path: 'test-results/upload-success.png' });
 });
